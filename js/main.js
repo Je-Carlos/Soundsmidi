4a892d76baf1f2500009c3557c3d2c6aa3889dc9
Jean <cpjeanberg@gmail.com>
2022-06-04 15:03:51 -0300

    Finalização da parte 2 - funcoes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,4 @@
-document.querySelector('.tecla_pom');
+function tocaSomPom(){
+    document.querySelector('#som_tecla_pom').play(); // está chamando o som da tecla pom
+}
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
