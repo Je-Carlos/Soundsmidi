479b992d2443fbd38c1b2908b19df9294c3e5550
Jean <cpjeanberg@gmail.com>
2022-06-05 19:56:12 -0300

    finalizando cap 4 + corrigindo bugs de som

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play(); // está chamando o som da tecla pom
+function tocaSom(idElementoAudio) {
+
+    document.querySelector(idElementoAudio).play(); // está chamando o som da tecla pom
 }
 const listaDeTeclas = document.querySelectorAll('.tecla'); // está pegando todas as teclas
 
-listaDeTeclas[0].onclick = tocaSomPom; // está  chamando a função tocaSomPom quando a tecla 0 for clicada 
+
+    for(let contador = 0; contador < listaDeTeclas.length; contador++){  // declarando ele como 0, e o contador vai ser menor que o tamanho da lista de teclas, e vai incrementando.   
+    const tecla = listaDeTeclas[contador];   // está criando um laço de repetição para que o contador seja menor que o tamanho da lista de teclas
+    const instrumento = tecla.classList[1]; 
+    const idAudio = `#som_${instrumento}`;
+   
+    tecla.onclick = function(){   
+        tocaSom(idAudio);
+    }; 
+}
